6cf5f4d4fdc55864538e918b7631759ef860ac1a
Carlos Moro García <carlosmoro95@gmail.com>
2016-05-18 17:39:29 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,8 @@
 var models = require('../models');
 
-// GET /quizzes
+// GET /quizzes	
 exports.index = function(req, res, next) {
-	models.Quiz.findAll().then(function(quizzes) {
+	models.Quiz.findAll({where: {question: {$like: "%" + req.query.search + "%"}}}).then(function(quizzes) {
 		res.render('quizzes/index.ejs', {quizzes: quizzes});
 	}).catch(function(error) {
 		next(error);
