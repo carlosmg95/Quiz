aaee7ba717b6cd1a262cfcf7ebe2cb3ee9315271
Carlos Moro García <carlosmoro95@gmail.com>
2016-05-19 17:25:19 +0200

    Tema 14: Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,5 @@
 var models = require('../models');
+var Sequelize = require('sequelize');
 
 // Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
@@ -86,8 +87,14 @@ exports.create = function(req, res, next) {
 	quiz.save({fields: ['question', 'answer']}).then(function(quiz) {
 		req.flash('success', 'Quiz creado con éxito');
 		res.redirect('/quizzes');	// res.redirect:
+	}).catch(Sequelize.ValidationError, function(error) {
+		req.flash('error', 'Errores en el formulario:');
+		for(var i in error.errors) {
+			req.flash('error', error.errors[i].value);
+		};
+		res.render('quizzes/new', {quiz: quiz});
 	}).catch(function(error) {		// redirección HTTP a lista de preguntas
-		req.flash('error', 'Error al crear un Quiz: ' + error.message);
+		req.flash('error', 'Error al crear un Quiz: ' + error);
 		next(error);
 	}); 
 }
